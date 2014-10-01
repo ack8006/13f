f9325e27558380eb4bcc8b454c1924b22f395e17
Alex Takata <takata.alex@gmail.com>
2014-10-01 00:28:07 -0400

    stuck on getting xpath

--- xmlScraper.py
+++ xmlScraper.py
@@ -1,2 +1,37 @@
 #13F scraper
 
+#http://www.sec.gov/cgi-bin/browse-edgar?CIK=0001167483&action=getcompany&type=13f
+#cik: is the firm identification number
+#action: needs to equal getcompany
+#type: 13f narrows it down
+#http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001167483&type=13f&start=0&count=40&output=atom
+#this is the RSS feed
+#provides the filing-date and filing-href
+
+from lxml import etree
+import lxml
+import requests
+
+import xml.etree.cElementTree as ET
+
+class xmlScraper():
+	def initializeScrape(self, cik):
+		self.get13FList(cik)
+
+	def get13FList(self, cik):
+		rssListString = "http://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=%s&type=13f&start=0&count=40&output=atom" % (cik)
+		page = requests.get(rssListString)
+		tree = etree.fromstring(page.content)
+
+		#STUCK HERE
+		authors = tree.xpath('*')
+		
+
+#will delete when functional
+def main():
+	xml = xmlScraper()
+	xml.initializeScrape("0001167483")
+
+
+if __name__ == "__main__":
+    main()
