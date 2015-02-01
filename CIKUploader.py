cdaa5814805af08ad0aa7f0a446986f1fb20e22e
Alex Takata <takata.alex@gmail.com>
2015-02-01 18:07:50 -0500

    pulls from the CIK CSV file

--- CIKUploader.py
+++ CIKUploader.py
@@ -1,1 +1,26 @@
 import MySQLdb
+import csv
+from contextlib import closing
+
+f = open('ciks.csv')
+csv_f = csv.reader(f)
+
+counter = 1
+db = MySQLdb.connect(host="127.0.0.1",user = "user1", passwd = "password", db="Quarterly13Fs")
+
+with closing(db.cursor()) as cur:
+	for row in csv_f:
+		firmName = None
+		CIK = None
+		if counter >1:
+			firmName = row[0]
+			CIK = row[1]
+			cur.execute("SELECT COUNT(*) as count FROM CIKList WHERE CIK = %s" %(CIK))
+			existingEntries = cur.fetchone()[0]
+			if existingEntries <1:
+				cur.execute("INSERT INTO CIKList (CIK, firmName) VALUES ('%s', '%s')" 
+					% (CIK, firmName))
+		counter += 1
+	db.commit()
+db.close()
+		
