68555e6ea373f5b303944fbdb521ff8d58c4fce7
Alex Takata <takata.alex@gmail.com>
2015-02-15 22:11:52 -0500

    implemented multiprocessing

--- UpdateScript.py
+++ UpdateScript.py
@@ -1,14 +1,20 @@
 from xmlScraper import UpdateChecker, Form13FUpdater
 import MySQLdb
 from contextlib import closing
-from datetime import datetime, date
-#from datetime import date
+from datetime import date
+import multiprocessing as mp
+
+def checkAndUpdate(cik, lastDate):
+	entries = upCheck.get13FList(cik, lastDate)
+	formUpdate = Form13FUpdater(cik, entries)
+	formUpdate.entryParser()
 
 db = MySQLdb.connect(host="127.0.0.1",user = "user1", passwd = "password", db="Quarterly13Fs")
 
 cikList = []
 
 with closing(db.cursor()) as cur:
+	#*****sort this by importance so the imporant ones go first...
 	cur.execute("SELECT CIK FROM CIKList")
 	cikList = [x[0] for x in cur.fetchall()]	
 
@@ -18,10 +24,16 @@ upCheck = UpdateChecker()
 #DAYS_TO_CHECK_FOR_UPDATES = 82
 DAYS_TO_CHECK_FOR_UPDATES =85
 
+#************RREAAAAAALLLLLYYY This should be multithreading,
+#might need to be multiple instances of UpdateChecker as opposed to just the one currently
+
+processes = []
 for cik in cikList:
 	lastDate = upCheck.mostRecentForm13F(cik)
-	print lastDate
+	print cik + ": " + str(lastDate)
 	if not lastDate or (lastDate and abs(date.today()-lastDate).days > DAYS_TO_CHECK_FOR_UPDATES):
-		entries = upCheck.get13FList(cik, lastDate)
-		formUpdate = Form13FUpdater(cik, entries)
-		formUpdate.entryParser()
+		processes.append(mp.Process(target=checkAndUpdate, args=(cik, lastDate)))
+
+for p in processes:
+	p.start()
+
